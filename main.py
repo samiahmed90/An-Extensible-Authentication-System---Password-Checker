eb01a4167da02e48adf755fb50a7d23032191560
samiahmed90 <mneykicks7@gmail.com>
2026-06-17 22:37:45 +0600

    Registration Functionality added.

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+def register_user():
+
+    username = input ("Please enter a Username:")
+    password = input ("Please enter a Password:")
+
+    with open("users.txt", "a") as user_file:
+        user_file.write(f"{username}: {password}\n")
+    
+    print("User Registration was Successful")
+
+
+
+
+
+
+
+
+
 def main():
     print("Welcome to the User Registration System")
     while True:
@@ -9,7 +27,7 @@ def main():
         choice = input("Please choose an option from 1-3:")
 
         if choice == "1":
-            print("Registration Selected")
+            register_user()
     
         elif choice == "2":
             print("Login Selected")
