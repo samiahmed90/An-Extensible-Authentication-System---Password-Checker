85894becc09e3d69a0c4f894c00938310b8a3674
samiahmed90 <mneykicks7@gmail.com>
2026-06-18 01:43:11 +0600

    Login completed

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
+# rgistration function
 def register_user():
 
     username = input ("Please enter a Username:")
     password = input ("Please enter a Password:")
 
     with open("users.txt", "a") as user_file:
-        user_file.write(f"{username}: {password}\n")
+        user_file.write(f"{username}:{password}\n")
     
     print("User Registration was Successful")
 
 
+# login function
 
+def login_user():
+    username = input ("Please enter a username:")
+    password = input ("Please enter a password:")
 
-
+    with open ("users.txt", "r") as user_file:
+        users = user_file.readlines()
+        
+    for user in users:
+        stored_username, stored_password = user.strip().split(":")
+        if username == stored_username and password == stored_password:
+            print("Login Successful")
+            return
+    print("Invalid Username or Password")
+    
 
 
 
@@ -30,7 +44,7 @@ def main():
             register_user()
     
         elif choice == "2":
-            print("Login Selected")
+            login_user()
 
         elif choice == "3":
             print("Exiting the system...")
